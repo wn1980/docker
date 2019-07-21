d00456260c3bfdad47f6f9e80e4de3f925c6f282
wn1980 <50045716+wn1980@users.noreply.github.com>
2019-07-22 02:20:42 +0700

    add sudo

--- dnn-gt640m-le/run.sh
+++ dnn-gt640m-le/run.sh
@@ -4,7 +4,6 @@ docker run --rm -it --init \
   --runtime=nvidia \
   --ipc=host \
   --publish 8888:8888 \
-  --user="$(id -u):$(id -g)" \
   --volume="$PWD:$HOME" \
   -e NVIDIA_VISIBLE_DEVICES=0 \
   wn1980/dnn-gt640m-le
