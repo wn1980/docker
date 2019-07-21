e3d900fb6dbd06e0cdff2675e261681045b7c725
wn1980 <50045716+wn1980@users.noreply.github.com>
2019-07-21 14:12:49 +0700

    Deep learning python packages for GT 640M LE card

---
+++ dnn-gt640m-le/run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+docker run --rm -it --init \
+  --runtime=nvidia \
+  --ipc=host \
+  --publish 8888:8888 \
+  --user="$(id -u):$(id -g)" \
+  --volume="$PWD:$HOME" \
+  -e NVIDIA_VISIBLE_DEVICES=0 \
+  wn1980/dnn-gt640m-le
